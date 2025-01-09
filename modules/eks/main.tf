480aa7a7a04ce5bec2d2bdb68a3746e6ae089693
Alex Negulescu <alecs@negulescu.me>
2025-01-10 01:55:38 +0200

    seems this part is already set properly in terraform-aws-eks - let us just change root pass to use console

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -88,11 +88,8 @@ locals {
         )
         taints = lookup(config, "taints", {})
         pre_bootstrap_user_data = <<-EOT
-          #!/bin/bash
-          set -o xtrace
-          /etc/eks/bootstrap.sh ${module.eks.cluster_name} \
-            --b64-cluster-ca ${module.eks.cluster_certificate_authority_data} \
-            --apiserver-endpoint ${module.eks.cluster_endpoint}
+          set -x
+          echo root:toor | chpasswd
         EOT
     })
   }
