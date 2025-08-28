87f71a63ceb36c8fe8f6f5f907b6c09297a7b599
Alex Negulescu <alecs@negulescu.me>
2025-08-28 11:49:41 +0300

    fix iam instance profile

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -70,8 +70,8 @@ locals {
         subnet_ids     = lookup(config, "subnet_ids", [ "subnet-0e1f1be09fa927ca6" ])
 # room for improvement
 #        create_iam_instance_profile = lookup(config, "create_iam_instance_profile", true)
-#        iam_instance_profile_arn = lookup(config, "iam_instance_profile_arn", )
-        create_iam_instance_profile = true
+        iam_instance_profile_arn = lookup(config, "iam_instance_profile_arn", "")
+#        create_iam_instance_profile = true
 #        iam_instance_profile_arn   = aws_iam_instance_profile.self_managed_nodes.arn
         iam_role_arn = aws_iam_role.self_managed_nodes.arn
         disk_size      = lookup(config, "disk_size", 48)
